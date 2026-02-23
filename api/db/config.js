c3cfb55a6f37fc2d3c21833c30ccf2c27aa33821
“Ayaan49” <ayaanbordoloi25@gmail.com>
2026-02-23 22:43:07 +0530

    Fix cinfig in api

--- api/db/config.js
+++ api/db/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || 3306,
-  username: process.env.DB_USER,
+  username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   dialect: 'mysql',
